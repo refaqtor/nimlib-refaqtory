3b937b19912f1d4962179221041d7f6dc3e7ef52
refaqtor <refaqtor@noemail.net>
2019-12-14 08:43:32 +0000

    rearrange for nimble and tests
    FossilOrigin-Name: 939e370d1654d1ca9681d77ca8f25f7e87cdf90f4a8a37a4c8459c4b8820ad5e

--- src/refaqtory/common.nim
+++ src/refaqtory/common.nim
@@ -1,1 +1,12 @@
 #common.nim
+
+#--- DISPLAY LEVEL
+
+var displayLevel: int
+# error = 0, state = 1, activity = 2, debug = 3
+proc setDisplay*(display = 1) =
+    displayLevel=display
+
+proc see*(level = 0, message: string) =
+    if level <= displayLevel:
+        echo message
