766e765403e64371a1909f961dcfa7535bba6220
refaqtor <refaqtor@noemail.net>
2019-12-14 08:56:14 +0000

    common basics
    FossilOrigin-Name: 80a2e91b11ba19ba1a6bf89b20de62f1e6b6b6df395a4c52b5bb36661d0dfd07

--- src/refaqtory/common.nim
+++ src/refaqtory/common.nim
@@ -1,5 +1,27 @@
 #common.nim
 
+import os, strutils, osproc
+
+# const respath* = "../res" / hostCPU / hostOS
+const configFolderName = ".refaqtory"
+const workTag* = "work"
+
+proc getPath*(rp: string) : string =
+    discard rp.existsOrCreateDir
+    assert existsDir rp
+    return rp
+
+proc configPath*() : string =
+    return getPath(unixToNativePath getHomeDir() / configFolderName)
+    
+proc projectPath*(project: string) : string =
+    return getPath(configPath() / project)
+    
+proc workPath*(project: string) : string =
+    return getPath(project.projectPath() / workTag)
+
+
+
 #--- DISPLAY LEVEL
 
 var displayLevel: int
@@ -10,3 +32,43 @@ proc setDisplay*(display = 1) =
 proc see*(level = 0, message: string) =
     if level <= displayLevel:
         echo message
+    if displayLevel > 3:
+        var log: File
+        if log.open(configPath()/"see.log", fmAppend):
+            log.writeLine(message)
+        defer:
+            log.close()
+
+proc runProcess*(cmd: string; wrkdir: string = ""): TaintedString  =
+    4.see "run cmd : " & cmd
+    let mycurdir = getCurrentDir()
+    if wrkdir != "":
+        setCurrentDir wrkdir
+    result = execProcess cmd
+    setCurrentDir mycurdir
+    return result
+
+
+# Version
+proc gitVer(): string=
+    let logfile = "gitlog.txt"
+    discard staticExec("git rev-parse HEAD > " & logfile)
+    logfile.readFile().splitLines()[0][0..8]
+    
+const versionTag* = gitVer()
+
+# const VcsInfo = staticExec("git rev-parse HEAD")
+const VcsInfo = staticExec("fossil info").split("checkout:")[1].strip()
+const thisVersion* = newConstVersion(
+    name="mynimall",
+    tag=versionTag,
+    date=CompileDate,
+    time=CompileTime,
+    vcs=VcsInfo[0..7],
+    extra="nim ver: " & NimVersion)
+
+proc newVersions*(): Versions =
+    result = @[newVersion(thisVersion)]
+
+var exeVersions* = newVersions()
+
